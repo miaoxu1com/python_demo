70d853e8873e807d344c97b7a90cf409cff4aaa2
miaoxu <miaoxu1com@users.noreply.github.com>
2024-09-05 12:54:42 +0800

    Update test_json_series.py

--- test_json_series.py
+++ test_json_series.py
@@ -1,1 +1,120 @@
-cysimdjson ujson orjson rapidjson hyperjson
+import time
+from functools import wraps
+
+import cysimdjson
+import json_lineage
+import msgspec
+import orjson
+import rapidjson
+import simdjson
+import simplejson as sjson
+import ujson
+
+fname = "large-file.json"
+
+
+def logit():
+    def logging_decorator(func):
+        @wraps(func)
+        def wrapped_function(*args, **kwargs):
+            start_time = time.time()
+            func(*args, **kwargs)
+            end_time = time.time()
+            print(end_time - start_time)
+
+        return wrapped_function
+
+    return logging_decorator
+
+
+# 耗时 0.281475305557251秒
+@logit()
+def read_json_by_simplejson(fname):
+    with open(fname, mode="r", encoding="utf-8") as f:
+        data = sjson.load(f)
+
+
+read_json_by_simplejson(fname)
+
+
+# 耗时 0.27578282356262207秒
+@logit()
+def read_json_by_ujson(fname):
+    with open(fname, mode="r", encoding="utf-8") as f:
+        data = ujson.load(f)
+
+
+read_json_by_ujson(fname)
+
+
+# 耗时 0.2868163585662842秒
+@logit()
+def read_json_by_rapidjson(fname):
+    with open(fname, mode="r", encoding="utf-8") as f:
+        data = rapidjson.load(f)
+
+
+read_json_by_rapidjson(fname)
+
+
+# 耗时 0.12167549133300781秒
+@logit()
+def read_json_by_orjson(fname):
+    with open(fname, mode="rb") as f:
+        data = orjson.loads(f.read())
+
+
+read_json_by_orjson(fname)
+
+
+# 耗时 0.11330294609069824秒
+@logit()
+def read_json_by_json_lineage(fname):
+    with open(fname, mode="r", encoding="utf-8") as f:
+        data = json_lineage.load(f.read())
+
+
+read_json_by_json_lineage(fname)
+
+parser = cysimdjson.JSONParser()
+
+
+# 耗时 0.03291010856628418秒  最快
+@logit()
+def read_json_by_json_cysimdjson(fname):
+    data = parser.load(fname)
+    # print(data)
+
+
+read_json_by_json_cysimdjson(fname)
+
+
+# 耗时 0.28174495697021484秒
+@logit()
+def read_json_by_json_simdjson(fname):
+    with open(fname, mode="r", encoding="utf-8") as f:
+        data = simdjson.load(f)
+
+
+read_json_by_json_simdjson(fname)
+
+
+class User(msgspec.Struct):
+    """A new type describing a User"""
+    name: str
+    groups: set[str] = set()
+    email: str | None = None
+
+
+alice = User("alice", groups={"admin", "engineering"})
+
+
+# 耗时 0.28174495697021484秒
+@logit()
+def read_json_by_json_msgspec():
+    msg = msgspec.json.encode(alice)
+
+
+read_json_by_json_msgspec()
+# libpy_simdjson pip安装报错
+# hyperjson 无人维护了
