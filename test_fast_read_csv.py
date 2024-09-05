eaf4ffbb8d5a66d2122684bcbf2f8d44f0f4b16c
miaoxu <miaoxu1com@users.noreply.github.com>
2024-09-05 14:20:03 +0800

    Update test_fast_read_csv.py

--- test_fast_read_csv.py
+++ test_fast_read_csv.py
@@ -1,5 +1,6 @@
 import time
 from functools import wraps
+from sys import getsizeof
 
 import numpy as np
 import pandas as pd
@@ -26,10 +27,11 @@ fname = "output.csv"
 # 耗时0.4268953800201416秒
 @logit()
 def pandas_read_csv():
-    values = pd.read_csv(fname,
-                         header=0,
-                         index_col=None)
-    return values
+    data = pd.read_csv(fname,
+                       header=0,
+                       index_col=None)
+    print(f"pandas default {getsizeof(data)} Bytes")
+    return data
 
 
 # 装饰器 中使用闭包函数，然后在函数中一层层返回，装饰器像是洋葱，洋葱要从外一层层的剥  装饰器是最外层的，所以就是先执行最外层装饰器函数，装饰器函数又引用了自定义的函数
@@ -40,12 +42,12 @@ pandas_read_csv()
 # 耗时0.0763709545135498秒
 @logit()
 def pandas_read_csv_by_pyarrow():
-    values = pd.read_csv(fname,
-                         engine="pyarrow",
-                         header=0,
-                         index_col=None)
-
-    return values
+    data = pd.read_csv(fname,
+                       engine="pyarrow",
+                       header=0,
+                       index_col=None)
+    print(f"pandas pyarrow {getsizeof(data)} Bytes")
+    return data
 
 
 pandas_read_csv_by_pyarrow()
@@ -54,12 +56,12 @@ pandas_read_csv_by_pyarrow()
 # 耗时3.424750566482544秒
 @logit()
 def pandas_read_csv_by_python():
-    values = pd.read_csv(fname,
-                         engine="python",
-                         header=0,
-                         index_col=None)
-
-    return values
+    data = pd.read_csv(fname,
+                       engine="python",
+                       header=0,
+                       index_col=None)
+    print(f"pandas python {getsizeof(data)} Bytes")
+    return data
 
 
 pandas_read_csv_by_python()
@@ -68,12 +70,12 @@ pandas_read_csv_by_python()
 # 耗时1.9833924770355225秒
 @logit()
 def pandas_read_csv_by_python_fwf():
-    values = pd.read_csv(fname,
-                         engine="python-fwf",
-                         header=0,
-                         index_col=None)
-
-    return values
+    data = pd.read_csv(fname,
+                       engine="python-fwf",
+                       header=0,
+                       index_col=None)
+    print(f"pandas python_fwf {getsizeof(data)} Bytes")
+    return data
 
 
 pandas_read_csv_by_python_fwf()
@@ -83,6 +85,7 @@ pandas_read_csv_by_python_fwf()
 @logit()
 def numpy_read_csv():
     data = np.array(np.loadtxt(fname, unpack=True, encoding='utf-8', skiprows=1, dtype=str))
+    print(f"numpy  {getsizeof(data)} Bytes")
 
 
 numpy_read_csv()
@@ -101,12 +104,13 @@ def polars_lazy_read_csv():
 
     # 收集并显示结果
     result_df.collect()
+    print(f"polars_lazy {getsizeof(result_df)} Bytes")
 
 
 polars_lazy_read_csv()
 
 
-# 耗时0.0554041862487793秒
+# 耗时0.0554041862487793秒  最快 内存最小
 @logit()
 def polars_lazy_streaming_read_csv():
     lazy_df = pl.scan_csv(fname)
@@ -118,8 +122,8 @@ def polars_lazy_streaming_read_csv():
     )
 
     # 懒惰操作并使用流式读取收集结果
-    result = result_df.collect(streaming=True)
-    # print(result)
+    data = result_df.collect(streaming=True)
+    print(f"polars_lazy_streaming  {getsizeof(data)} Bytes")
 
 
 polars_lazy_streaming_read_csv()
