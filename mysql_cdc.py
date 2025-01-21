82ba242d47a2aa9e64d2e3fd5386803b21a6bf14
miaoxu <miaoxu1com@users.noreply.github.com>
2025-01-21 17:29:54 +0800

    Update mysql_cdc.py

--- mysql_cdc.py
+++ mysql_cdc.py
@@ -5,6 +5,8 @@ from pymysqlreplication import BinLogStreamReader
 from pymysqlreplication.row_event import WriteRowsEvent
 
 #由于监控捕获的模块和发送模块处理速度并不一致，发送需要耗时可能产生阻塞，而捕获数据是实时的，所以要进行解耦，使用两个进程进行处理
+# https://docs.python.org/3.6/library/multiprocessing.html#multiprocessing.SimpleQueue
+# 这里就要使用多进程之间的通信机制了 ipc 
 
 
 #1. 使用 MySQL 的触发器 + 消息队列
